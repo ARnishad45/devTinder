43bfd6ea10fe9690ade843389ed9fae51bb70076
ARnishad45 <arnishad4518@gmail.com>
2025-02-25 19:29:00 +0530

    Updated middleware

--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -1,13 +1,30 @@
-const userAuth = ('/user', (req, res, next) => {
-    console.log("Authentication middleware called")
-    const token = "xyz";
-    const isAuthenticated = token === "xyz";
-    if(token != "xyz") {
-        req.status(401).send("Invalid request token");
-    }
-    else{
+const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+
+const userAuth = ('/user', async (req, res, next) => {
+    try{
+        const { token } = req.cookies;
+        if(!token){
+            throw new Error("Invalid token!!!");
+        }
+        
+        const decodeToken = await jwt.verify(token, "#Dev@Tinder$AR45");
+        
+        const {_id} = decodeToken;
+        
+        const user = await User.findById(_id);
+        console.log(user);
+
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+
+        req.user = user; //attaching user to request object
         next();
+    } catch (error) {
+        res.status(400).send("Error: " + error.message);
     }
+
 });
 
 module.exports = {userAuth}
