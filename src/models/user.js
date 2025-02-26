645b6d60e6ba319b2a54a8c68738d5971a290739
ARnishad45 <arnishad4518@gmail.com>
2025-02-26 12:23:21 +0530

    Updated User Schema

--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,8 @@ const userSchema = new mongoose.Schema({
         type : 'string',
         required : true,
         minLength : 3,
-        maxLength : 50
+        maxLength : 50,
+        index: true, //makes querry faster
     },
     lastName : {
         type : 'string',
@@ -68,6 +69,9 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
 }
 );
+
+userSchema.index({firstName: 1, lastName: 1});
+userSchema.index({gender: 1});
 
 userSchema.methods.getJWT = async function(){
     const user = this;
